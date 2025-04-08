9e57cd341033aa6fb7d1dd0ea971d8e28c20c47b
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-04-08 20:56:20 +0000

    COMPLETION

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,10 +4,32 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+	
+			public static int[] insertionSort(double[] nums) {
+				int swaps = 0;
+				int steps = 0;
+				
+				for (int i = 1; i < nums.length; i++) {
+					double key = nums[i];
+					int j = i - 1;
+					steps++; 
+					
+					while (j >= 0 && nums[j] > key) {
+						steps++; 
+						nums[j + 1] = nums[j];
+						swaps += 1; 
+					}
+					nums[j + 1] = key;
+					swaps += 1; 
+				}
+				
+				return new int[]{swaps, steps};
+			}
+			
+			
+		
 
+	
 	public static void q2() {
 		//Write question 2 code here
 	}
